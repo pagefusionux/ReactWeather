b7cac7bc88af475f3053cc2be50ded5b2702b9b6
Chris Davis <chris.davis@clearlink.com>
2017-03-14 13:47:30 -0600

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ var express = require('express');
 
 // create our app
 var app = express();
+const port = process.env.PORT || 3000; // for deploying to Heroku
+
+// fix for redirecting https URL for Weather API
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-   console.log('Express server is up on port 3000'); 
+app.listen(port, function() {
+   console.log('Express server is up on port ' + port);
 });

301301a300a3a806c9e0dc86861365f49043a3c9
Chris Davis <chris.davis@clearlink.com>
2017-03-21 13:40:27 -0600

    Updated About page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -18,8 +18,25 @@ var About = React.createClass({
 const About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is the about component.</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This application was made by following a tutorial called "The Complete React Web App Developer Course" on
+        Udemy.com.
+      </p>
+      <ul>
+        <li>
+          <a href="http://www.udemy.com" target="_blank">Udemy</a>
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/sites/docs" target="_blank">Foundation Docs</a>
+        </li>
+        <li>
+          <a href="http://facebook.github.io/react/docs" target="_blank">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org" target="_blank">OpenWeatherMap.org</a>
+        </li>
+      </ul>
     </div>
   );
 };
